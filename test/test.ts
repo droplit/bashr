c49a66e31eb34484f80922e37f48176d6e385661
Chris Woodle <chris@droplit.io>
2018-08-19 17:45:31 -0400

    cli spawn test

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,4 @@
-import { spawnSync } from 'child_process';
+const spawn = require('cross-spawn');
 import * as path from 'path';
 import 'mocha';
 import { expect } from 'chai';
@@ -262,22 +262,28 @@ describe('Params', function () {
 
 describe('Child process test', function () {
     this.slow(500);
+    before(() => {
+        const command = spawn.sync('npm', ['link'], { cwd: path.join(__dirname, './food') });
+    });
     it('Run food cli "hello"', done => {
-        const command = spawnSync(path.join(__dirname, './food/bin/food'), ['hello']);
+        const command = spawn.sync('food', ['hello']);
         const output = command.stdout.toString();
         expect(output).to.equal('world');
         done();
     });
     it('Run food cli "fruit banana peel"', done => {
-        const command = spawnSync(path.join(__dirname, './food/bin/food'), ['fruit', 'banana', 'peel']);
+        const command = spawn.sync('food', ['fruit', 'banana', 'peel']);
         const output = command.stdout.toString();
         expect(output).to.equal('peeled!');
         done();
     });
     it('Food cli default "fruit banana peel foo"', done => {
-        const command = spawnSync(path.join(__dirname, './food/bin/food'), ['fruit', 'banana', 'peel', 'foo']);
+        const command = spawn.sync('food', ['fruit', 'banana', 'peel', 'foo']);
         const output = command.stdout.toString();
         expect(output).to.equal('default handler');
         done();
     });
+    after(() => {
+        spawn.sync('npm', ['unlink', 'food']);
+    })
 });
